eb6bdf6f571b5d83ce2c0ca503c52cfead7456ae
gene courtney <genecourtney7@gmail.com>
2023-12-07 22:41:52 -0600

    had to do some dubugging it wasnt producing a price. Now it is

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,45 +1,42 @@
-// Business Logic
-
 class Pizza {
-  constructor(crust,toppings, size) {
-    this.crust = crust;
-    this.toppings = toppings;
-    this.size = size;
-  }
-
-  price() {
-    let cost = 0;
-    if (this.size == "small") {
-      cost += 5;
+    constructor(toppings, size) {
+        this.toppings = toppings;
+        this.size = size;
     }
-    else if (this.size == "medium"){
-      cost += 7;
-    }
-    else {
-      cost += 10;
-    }
-    cost += .25 * this.toppings.length;
-    return cost;
-  }
-}
-
-
-//UI Logic
-window.onload = function () {
-  const orderButton = document.getElementById("order");
-  orderButton.addEventListener("click", function (event){
-    event.preventDefault();
-    const toppings = [];
-    const toppingsCheckboxes = document.querySelectorAll("input[type=checkbox]");
-    for (let i = 0, i < toppingsCheckboxes.length; i++) {
-        if (toppingsCheckboxes[i].checked) {
-            toppings.push(toppingsCheckboxes[i].id);
+    price() {
+        let cost = 0;
+        if (this.size == "small") {
+            cost += 5;
         }
+        else if (this.size == "medium") {
+            cost += 7;
+        }
+        else {
+            cost += 10;
+        }
+        cost += .25 * this.toppings.length;
+        return cost;
     }
-    const sizeSelect = document.getElementById("size");
-    const size = sizeSelect.options[sizeSelect.selectedIndex].value;
-    const pizza = new Pizza(crust, toppings, size);
-    const resultsDiv = document.getElementById("results");
-    resultsDiv.innerHTML = `Your pizza cost $${pizza.price()}`;
-  });
-};
+  
+  }
+    
+   
+  //UI Logic
+  window.onload = function () {
+    const orderButton = document.getElementById("order");
+    orderButton.addEventListener("click", function (event) {
+      event.preventDefault();
+      const toppings = [];
+      const toppingsCheckboxes = document.querySelectorAll("input[type=checkbox]");
+      for (let i = 0; i < toppingsCheckboxes.length; i++) {
+        if (toppingsCheckboxes[i].checked) {
+          toppings.push(toppingsCheckboxes[i].id);
+        }
+      }
+      const sizeSelect = document.getElementById("size");
+      const size = sizeSelect.options[sizeSelect.selectedIndex].value;
+      const pizza = new Pizza(toppings, size);
+      const resultsDiv = document.getElementById("results");
+      resultsDiv.innerHTML = `Your pizza costs $${pizza.price()}`;
+    });
+  };
