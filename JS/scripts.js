213bc2aa96508a7bb51b1ffbe1b7901d03dc5145
gene courtney <genecourtney7@gmail.com>
2023-12-07 21:32:31 -0600

    added a price function to the pizza object

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,9 +1,27 @@
 // Business Logic
 
 class Pizza {
-    constructor(crust,toppings, size) {
-        this.crust = crust;
-        this.toppings = toppings;
-        this.size = size;
+  constructor(crust,toppings, size) {
+    this.crust = crust;
+    this.toppings = toppings;
+    this.size = size;
+  }
+
+  price() {
+    let cost = 0;
+    if (this.size == "small") {
+      cost += 5;
     }
+    else if (this.size == "medium"){
+      cost += 7;
+    }
+    else {
+      cost += 10;
+    }
+    cost += .25 * this.toppings.length;
+    return cost;
+  }
 }
+
+
+//UI Logic
