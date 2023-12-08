0140490323913086e2be90820390b985b3e9b3b4
gene courtney <genecourtney7@gmail.com>
2023-12-07 21:52:29 -0600

    added the ui logic

--- JS/scripts.js
+++ JS/scripts.js
@@ -25,3 +25,21 @@ class Pizza {
 
 
 //UI Logic
+window.onload = function () {
+  const orderButton = document.getElementById("order");
+  orderButton.addEventListener("click", function (event){
+    event.preventDefault();
+    const toppings = [];
+    const toppingsCheckboxes = document.querySelectorAll("input[type=checkbox]");
+    for (let i = 0, i < toppingsCheckboxes.length; i++) {
+        if (toppingsCheckboxes[i].checked) {
+            toppings.push(toppingsCheckboxes[i].id);
+        }
+    }
+    const sizeSelect = document.getElementById("size");
+    const size = sizeSelect.options[sizeSelect.selectedIndex].value;
+    const pizza = new Pizza(crust, toppings, size);
+    const resultsDiv = document.getElementById("results");
+    resultsDiv.innerHTML = `Your pizza cost $${pizza.price()}`;
+  });
+};
