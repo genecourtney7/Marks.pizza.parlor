ba11ee21c12b9566d418f69001d00f582c8e713f
gene courtney <genecourtney7@gmail.com>
2023-12-09 22:51:42 -0600

    ive added an add to order button and function to add multiple pizzas to the order

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,42 +1,56 @@
 class Pizza {
-    constructor(toppings, size) {
-        this.toppings = toppings;
-        this.size = size;
-    }
-    price() {
-        let cost = 0;
-        if (this.size == "small") {
-            cost += 5;
-        }
-        else if (this.size == "medium") {
-            cost += 7;
-        }
-        else {
-            cost += 10;
-        }
-        cost += .25 * this.toppings.length;
-        return cost;
-    }
-  
+  constructor(toppings, size) {
+    this.toppings = toppings;
+    this.size = size;
   }
+  price() {
+    let cost = 0;
+    if (this.size == "small") {
+      cost += 5;
+    }
+    else if (this.size == "medium") {
+      cost += 7;
+    }
+    else {
+      cost += 10;
+    }
+    cost += .25 * this.toppings.length;
+    return cost;
+  }
+}
+  
+let order = [];
+  
+//UI Logic
+window.onload = function () {
+  const orderButton = document.getElementById("order");
+  const addToOrderButton = document.getElementById("addToOrder");
     
-   
-  //UI Logic
-  window.onload = function () {
-    const orderButton = document.getElementById("order");
-    orderButton.addEventListener("click", function (event) {
-      event.preventDefault();
-      const toppings = [];
-      const toppingsCheckboxes = document.querySelectorAll("input[type=checkbox]");
-      for (let i = 0; i < toppingsCheckboxes.length; i++) {
-        if (toppingsCheckboxes[i].checked) {
-          toppings.push(toppingsCheckboxes[i].id);
-        }
+  addToOrderButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    const toppings = [];
+    const toppingsCheckboxes = document.querySelectorAll("input[type=checkbox]");
+    for (let i = 0; i < toppingsCheckboxes.length; i++) {
+      if (toppingsCheckboxes[i].checked) {
+        toppings.push(toppingsCheckboxes[i].id);
       }
-      const sizeSelect = document.getElementById("size");
-      const size = sizeSelect.options[sizeSelect.selectedIndex].value;
-      const pizza = new Pizza(toppings, size);
-      const resultsDiv = document.getElementById("results");
-      resultsDiv.innerHTML = `Your pizza costs $${pizza.price()}`;
-    });
-  };
+    }
+    const sizeSelect = document.getElementById("size");
+    const size = sizeSelect.options[sizeSelect.selectedIndex].value;
+    const pizza = new Pizza(toppings, size);
+    order.push(pizza);
+    const resultsDiv = document.getElementById("results");
+    resultsDiv.innerHTML = `A ${size} pizza with ${toppings.join(", ")} added to order. Total pizzas in order: ${order.length}`;
+  });
+    
+  orderButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    let totalCost = 0;
+    for (let i = 0; i < order.length; i++) {
+      totalCost += order[i].price();
+    }
+    const resultsDiv = document.getElementById("results");
+    resultsDiv.innerHTML = `Your order costs $${totalCost.toFixed(2)}`;
+  });
+};
+  
